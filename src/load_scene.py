1547949468409ef11f258e776f9d07c851bef376
emcaruso <emcaruso@unibz.it>
2024-04-09 18:41:58 +0200

    chk

--- src/load_scene.py
+++ src/load_scene.py
@@ -2,18 +2,18 @@ import os, sys
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(script_dir,"src"))
-from load_from_blender import load_from_blender
-from load_from_calib import load_from_calib
+from LightCalib import load_light_calib_data
+from utils_ema.config_utils import load_yaml
 
 
+def load_scene():
+    cfg = load_yaml(os.path.join(script_dir,"..","configs", "light_calib.yaml"))
+    lcl = load_light_calib_data.LightCalibLoader(cfg)
+    # point_lights = lcl.get_point_lights()
+    # scene = lcl.get_scene_only_lights()
+    scene = lcl.get_scene()
+    scene.plot()
+    return scene
 
-class SceneLoader():
-
-    @staticmethod
-    def load_from_calib(cfg_calib):
-        scene = load_from_calib(cfg_calib)
-        return scene
-
-    @staticmethod
-    def load_from_blender():
-        pass
+if __name__=="__main__":
+    load_scene()
