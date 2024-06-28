268e89c2477e08fc2543477def801e7846d9de46
emcaruso <emcaruso@unibz.it>
2024-06-28 19:55:01 +0200

    chk2

--- scripts/calibrate.sh
+++ scripts/calibrate.sh
@@ -1,3 +1,4 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 python3 $SCRIPTPATH/../src/Calibration/src/main.py --config_path $SCRIPTPATH/../configs/calibration.yaml
+python3 $SCRIPTPATH/../src/Calibration/src/conveyor_preprocess.py --config_path $SCRIPTPATH/../configs/calibration.yaml
--- scripts/mitsuba_rend.sh
+++ scripts/mitsuba_rend.sh
@@ -1,3 +1,3 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-python3 $SCRIPTPATH/../src/MitsubaRenderer/src/renderer.py --config_path $SCRIPTPATH/../configs/mitsuba_rend.yaml
+python3 $SCRIPTPATH/../src/MitsubaRenderer/src/solver.py --config_path $SCRIPTPATH/../configs/mitsuba_rend.yaml
