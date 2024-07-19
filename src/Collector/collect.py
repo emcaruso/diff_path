6780648d8b079341b46be81c5fef3a850a40ccda
emcaruso <emcaruso@unibz.it>
2024-07-19 11:41:38 +0200

    chk

--- src/Collector/collect.py
+++ src/Collector/collect.py
@@ -1,5 +1,6 @@
 import os, sys
 import numpy as np
+import torch
 import time
 import shutil
 from omegaconf import OmegaConf
@@ -36,6 +37,34 @@ class Collector():
         self.lights = self.scene.get_lights()
         if len(self.cams) != self.fe.n_devices: raise ValueError("different number of devices wrt number of cameras in scene!")
 
+
+    def prepare_normalization_data(self):
+        self.cc_gain = torch.ones((self.fe.n_devices, 3))
+        if self.cfg.normalization.color_correction:
+            d = load_yaml(os.path.join(self.cfg.paths.color_correction_data_dir,"res.yaml"))
+            for i, (k, v) in enumerate(d.items()):
+                self.cc_gain[i,:] = torch.tensor(v,dtype=torch.float32)
+
+        self.exp_gain = torch.ones(self.fe.n_devices)
+        if self.cfg.normalization.exposure_time_ref is not None:
+            ets = self.fe.get_exposure()
+            self.exp_gain = self.cfg.normalization.exposure_time_ref/torch.tensor(ets, dtype=torch.float32)
+
+    def preprocess(self, images):
+
+        # undistort images
+        images_undist = [ self.cams[i].intr.undistort_image(images[i]) for i in range(len(images)) ]
+
+        # normalize image colors
+        for i, image in enumerate(images_undist):
+            image.img *=self.cc_gain[i,:] # color correction normalization
+            image.img *=self.exp_gain[i] # exposure normalization
+            image.img = np.clip(image.img, 0, 0.999)
+
+        return images_undist
+
+
+
     def test_leds(self):
         for i in range(self.led_cfg.test.n):
             for k,v in self.led_cfg.leds.items():
@@ -45,11 +74,13 @@ class Collector():
 
     def show_references( self ):
         self.fe.start_cams()
+        self.prepare_normalization_data()
         idx = 0
         while True:
             while True:
                 images = self.fe.grab_multiple_cams()
-                images_undist = [ self.cams[i].intr.undistort_image(images[i]) for i in range(len(images)) ]
+                # images_undist = [ self.cams[i].intr.undistort_image(images[i]) for i in range(len(images)) ]
+                images_undist = self.preprocess(images)
                 images_overlap = self.pe.overlap_pose_from_idx( images_undist, idx)
                 key = Image.show_multiple_images(images_overlap, wk=1)
                 if key==ord('q'):
@@ -89,11 +120,14 @@ class Collector():
         launch_blender_script(blend_file, os.path.join(script_dir,"save_blender.py"), arguments=[self.cfg.paths.save_dir])
         # launch_blender_script(blend_file, os.path.join(script_dir,"save_blender.py"), arguments=[os.path.join(self.cfg.paths.pose_estimator_data_dir,"config.yaml")])
 
-    def capture_frames_with_led( self, led ):
-        self.lc.led_on(led, 1)
-        images = self.fe.grab_multiple_cams()
-        self.lc.led_off(led)
-        return images
+    # def capture_frames_with_led( self, led ):
+    #     exp_time = self.fe.get_exposure()
+    #     self.lc.led_on(led, 1)
+    #     time.sleep(0.01)
+    #     images = self.fe.grab_multiple_cams()
+    #     time.sleep(max(exp_time)*1e-6)
+    #     self.lc.led_off(led)
+    #     return images
 
 
     def save_images(self, image_list, image_show_list):
@@ -103,15 +137,21 @@ class Collector():
         shutil.rmtree( out_dir, ignore_errors=True )
         os.makedirs( out_dir )
 
+        if image_list == []:
+            return False
+
+
         for j in range(len(image_list[0])):
             o_dir = os.path.join(out_dir,"sequences","Cam_"+str(j).zfill(3))
             o_dir_show = os.path.join(out_dir,"sequences_show","Cam_"+str(j).zfill(3))
             os.makedirs(o_dir)
             os.makedirs(o_dir_show)
             for i in range(len(image_list)):
+                image_list[i][j].type(torch.uint8)
                 image_list[i][j].save( os.path.join(o_dir,str(i).zfill(3)+".png" ))
                 image_show_list[i][j].save( os.path.join(o_dir_show,str(i).zfill(3)+".png"))
 
+        return True
 
 
     # def sequence_grab( self, images_undist, imgs_show, pose_list, image_list, image_show_list, led_list, pose, sequence, rang ):
@@ -147,6 +187,8 @@ class Collector():
 
     def save_data(self, pose_list, led_list):
         # SAVE
+        if pose_list == []:
+            return False
 
         # save data
         out_dir = self.cfg.paths.captured_dir
@@ -162,6 +204,8 @@ class Collector():
         cfg_path_out = os.path.join(self.cfg.paths.save_dir, "config.yaml")
         OmegaConf.save(self.cfg, cfg_path_out)
     
+        return True
+
     # def save_data(self, pose_list, image_list, led_list, image_show_list):
     #     # SAVE
     #     # save data
@@ -189,28 +233,26 @@ class Collector():
         if key==ord('q'):
             return False
         else:
-
-
             x = pose.location()[0]
             x_ref = sequence[0][1]
 
             if x>=x_ref:
                 light_id = sequence[0][0]
                 sequence.pop(0)
-                images = self.capture_frames_with_led( light_id)
-            
-                # capture frame and switch led
                 images = self.fe.grab_multiple_cams()
-                self.lc.led_off(light_id)
-                if sequence: self.lc.led_on(sequence[0][0])
+                if sequence: self.lc.led_on(sequence[0][0], self.cfg.led_amp)
 
-                images_undist = [ self.cams[i].intr.undistort_image(images[i]) for i in range(len(images)) ]
+                # images_undist = [ self.cams[i].intr.undistort_image(images[i]) for i in range(len(images)) ]
+                images_undist = self.preprocess(images)
                 imgs_show, pose, _, _ = self.pe.overlap_estimated_pose(images_undist, rang=rang)
                 pose_list.append(pose)
                 image_list.append(images_undist)
                 image_show_list.append(imgs_show)
-
                 led_list.append([])
+
+                # key = Image.show_multiple_images(imgs_show, wk=1) # FOR DEBUG
+                self.lc.led_off(light_id)
+
                 if self.lights is not None:
                     for l_id in range(len(self.lights)):
                         intensity = (l_id==light_id)*self.lc.ampere_default
@@ -220,6 +262,7 @@ class Collector():
                 # led_list.append({"light_id":light_id, "intensity":self.lc.ampere_default})
                 else:
                     print(f"collected {len(image_list)} images")
+
 
                 if len(sequence)==0:
                     return False
@@ -241,8 +284,10 @@ class Collector():
             # led_list.append({"light_id":-1, "intensity":-1, "name":self.lights[]})
         if leds and key in [ord('0'), ord('1'), ord('2'), ord('3'), ord('4'), ord('5'), ord('6'), ord('7'), ord('8'), ord('9')]:
             light_id = key-48
-            images = self.capture_frames_with_led( light_id)
-            images_undist = [ self.cams[i].intr.undistort_image(images[i]) for i in range(len(images)) ]
+            # images = self.capture_frames_with_led( light_id)
+            images = self.fe.grab_multiple_cams()
+            # images_undist = [ self.cams[i].intr.undistort_image(images[i]) for i in range(len(images)) ]
+            images_undist = self.preprocess(images)
             imgs_show, pose, _, _ = self.pe.overlap_estimated_pose(images_undist, rang=rang)
             pose_list.append(pose)
             image_list.append(images_undist)
@@ -256,17 +301,22 @@ class Collector():
     def collect_manual( self ):
         # collect
         self.fe.start_cams(signal_period=self.cfg.collect_manual.signal_period)
+        self.prepare_normalization_data()
         image_list = []
         image_show_list = []
         pose_list = []
         led_list = []
         while True:
             images = self.fe.grab_multiple_cams()
-            images_undist = [ self.cams[i].intr.undistort_image(images[i]) for i in range(len(images)) ]
+            # images_undist = [ self.cams[i].intr.undistort_image(images[i]) for i in range(len(images)) ]
+            images_undist = self.preprocess(images)
             imgs_show, pose, _, _= self.pe.overlap_estimated_pose(images_undist)
 
             f = self.manual_grab( images_undist, imgs_show, pose_list, image_list, image_show_list, led_list, pose)
             if not f: break
+
+        for led in self.led_cfg.leds.values():
+            self.lc.led_off(led.channel)
 
         self.save_data(pose_list, image_list, led_list, image_show_list)
 
@@ -274,6 +324,7 @@ class Collector():
     def collect_while_tracking( self, manual=True, debug=False ) :
         # collect
         self.fe.start_cams(signal_period=self.cfg.collect_track.signal_period, exposure_time=self.cfg.collect_track.exposure_time)
+        self.prepare_normalization_data()
         image_list = []
         image_show_list = []
         pose_list = []
@@ -285,9 +336,10 @@ class Collector():
         w = self.cfg.collect_track.range_half
         while True:
             images = self.fe.grab_multiple_cams()
-            images_undist = [ self.cams[i].intr.undistort_image(images[i]) for i in range(len(images)) ]
+            # images_undist = [ self.cams[i].intr.undistort_image(images[i]) for i in range(len(images)) ]
+            images_undist = self.preprocess(images)
             rang = [max(0,current_id-w), min(current_id+w,self.pe.n_poses-1)]
-            imgs_show, pose, in_thresh, best_pose_id = self.pe.overlap_estimated_pose(images_undist, rang=rang)
+            imgs_show, pose, in_thresh, best_pose_id = self.pe.overlap_estimated_pose(images_undist, rang=rang, debug=debug)
 
             if in_thresh:
                 current_id = best_pose_id
@@ -300,17 +352,21 @@ class Collector():
             else:
                 if sequence is None: sequence = self.cfg.collect_sequence.sequence
                 if sequence is None: raise ValueError("sequence in config file is empty")
-                self.lc.led_on(sequence[0][0])
+                self.lc.led_on(sequence[0][0], self.cfg.led_amp)
                 f = self.sequence_grab( images_undist, imgs_show, pose_list, image_list, image_show_list, led_list, pose, sequence, rang )
                 if not f:
                     cv2.destroyAllWindows()
                     break
 
 
+        for led in self.led_cfg.leds.values():
+            self.lc.led_off(led.channel)
+
         # save data loader
         
         self.save_images(image_list, image_show_list)
         self.save_data(pose_list, led_list)
+
 
         return True
 
--- src/Collector/show_reference.py
+++ src/Collector/show_reference.py
@@ -1,5 +1,19 @@
-
+import os
+from utils_ema.config_utils import load_yaml
+from utils_ema.light_control import LightController
 from collect import Collector
+
+script_dir = os.path.abspath(os.path.dirname(__file__))
+led_cfg_path = os.path.join(script_dir, "..", "..", "configs", "leds.yaml")
+led_cfg = load_yaml(led_cfg_path)
+
+lc = LightController(ip_controller=led_cfg.ip_controller)
+
+for k,v in led_cfg.leds.items():
+    lc.led_on(channel=v.channel, amp=0.4)
 
 c = Collector()
 c.show_references()
+
+for k,v in led_cfg.leds.items():
+    lc.led_off(channel=v.channel)
